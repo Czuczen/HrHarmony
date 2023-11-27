633130b80d17a501904def7e3b11890e88e1d43a
Czuczen <czuczengames@gmail.com>
2023-11-27 19:11:21 +0100

    Widoki: Home. Zmiana logiki renderowania kart. UNIFIKACJA

--- HrHarmony/Controllers/HomeController.cs
+++ HrHarmony/Controllers/HomeController.cs
@@ -1,22 +1,139 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using HrHarmony.Data.Models.ViewModels;
+using HrHarmony.Data.Models.ViewModels.Home;
 
 namespace HrHarmony.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
-  
-        return View();
+        var ret = new List<IndexViewModel>
+        {
+            new IndexViewModel
+            {
+                ImagePath = "/Content/Pracownicy.jpg",
+                ImageAlt = "Zarządzaj pracownikami",
+                FrontTitle = "Pracownicy",
+                FrontText = "",
+                BackSideTitle = "System zarządzania pracownikami",
+                BackSideText = "Zarządzaj swoim zespołem efektywnie dzięki naszemu systemowi do zarządzania pracownikami.",
+                EntityName = "Employee",
+            },
+            new IndexViewModel
+            {
+                ImagePath = "/Content/Umowy.jpg",
+                ImageAlt = "Zarządzaj umowami",
+                FrontTitle = "Umowy",
+                FrontText = "",
+                BackSideTitle = "System zarządzania umowami",
+                BackSideText = "System zarządzania umowami pozwoli Ci skutecznie monitorować i organizować dokumenty związane z zatrudnieniem.",
+                EntityName = "EmploymentContract",
+            },
+            new IndexViewModel
+            {
+                ImagePath = "/Content/Urlopy.jpg",
+                ImageAlt = "Zarządzaj urlopami",
+                FrontTitle = "Urlopy",
+                FrontText = "",
+                BackSideTitle = "System zarządzania urlopami",
+                BackSideText = "Skorzystaj z naszego systemu zarządzania urlopami, aby efektywnie planować i monitorować czas wolny pracowników.",
+                EntityName = "Leave",
+            },
+            new IndexViewModel
+            {
+                ImagePath = "/Content/Absencje.jpeg",
+                ImageAlt = "Zarządzaj absencjami",
+                FrontTitle = "Absencje",
+                FrontText = "",
+                BackSideTitle = "System zarządzania absencjami",
+                BackSideText = "Zarządzaj absencjami pracowników w łatwy sposób za pomocą naszego systemu do śledzenia i raportowania.",
+                EntityName = "Absence",
+            },
+            new IndexViewModel
+            {
+                ImagePath = "/Content/Wynagrodzenia.jpg",
+                ImageAlt = "Zarządzaj wynagrodzeniami",
+                FrontTitle = "Wynagrodzenia",
+                FrontText = "",
+                BackSideTitle = "System zarządzania wynagrodzeniami",
+                BackSideText = "System zarządzania wynagrodzeniami umożliwi Ci efektywne monitorowanie i administrację wynagrodzeniami pracowników.",
+                EntityName = "Salary",
+            },
+            new IndexViewModel
+            {
+                ImagePath = "/Content/Others.png",
+                ImageAlt = "Zarządzaj rodzajami absencji",
+                FrontTitle = "Rodzaje absencji",
+                FrontText = "",
+                BackSideTitle = "System zarządzania rodzajami absencji",
+                BackSideText = "Sprawdź nasz system zarządzania rodzajami absencji, aby skutecznie kategoryzować i monitorować nieobecności pracowników.",
+                EntityName = "AbsenceType",
+            },
+            new IndexViewModel
+            {
+                ImagePath = "/Content/Others.png",
+                ImageAlt = "Zarządzaj adresami",
+                FrontTitle = "Adresy",
+                FrontText = "",
+                BackSideTitle = "System zarządzania adresami",
+                BackSideText = "Efektywnie zarządzaj informacjami adresowymi pracowników dzięki naszemu systemowi zarządzania adresami.",
+                EntityName = "Address",
+            },
+            new IndexViewModel
+            {
+                ImagePath = "/Content/Others.png",
+                ImageAlt = "Zarządzaj rodzajami umów",
+                FrontTitle = "Rodzaje umów",
+                FrontText = "",
+                BackSideTitle = "System zarządzania rodzajami umów",
+                BackSideText = "Poznaj różnorodność umów pracowniczych za pomocą naszego systemu zarządzania rodzajami umów.",
+                EntityName = "ContractType",
+            },
+            new IndexViewModel
+            {
+                ImagePath = "/Content/Others.png",
+                ImageAlt = "Zarządzaj poziomami wykształcenia",
+                FrontTitle = "Poziomy wykształcenia",
+                FrontText = "",
+                BackSideTitle = "System zarządzania poziomami wykształcenia",
+                BackSideText = "System zarządzania poziomami wykształcenia umożliwi Ci skuteczne monitorowanie kwalifikacji edukacyjnych pracowników.",
+                EntityName = "EducationLevel",
+            },
+            new IndexViewModel
+            {
+                ImagePath = "/Content/Others.png",
+                ImageAlt = "Zarządzaj doświadczeniami",
+                FrontTitle = "Doświadczenia",
+                FrontText = "",
+                BackSideTitle = "System zarządzania doświadczeniami",
+                BackSideText = "Zarządzaj doświadczeniem zawodowym pracowników za pomocą naszego systemu śledzenia kariery zawodowej.",
+                EntityName = "Experience",
+            },
+            new IndexViewModel
+            {
+                ImagePath = "/Content/Others.png",
+                ImageAlt = "Zarządzaj rodzajami urlopów",
+                FrontTitle = "Rodzaje urlopów",
+                FrontText = "",
+                BackSideTitle = "System zarządzania rodzajami urlopów",
+                BackSideText = "Skorzystaj z systemu zarządzania rodzajami urlopów, aby dokładnie śledzić i planować różne kategorie urlopów.",
+                EntityName = "LeaveType",
+            },
+            new IndexViewModel
+            {
+                ImagePath = "/Content/Others.png",
+                ImageAlt = "Zarządzaj stanami cywilnymi",
+                FrontTitle = "Rodzaje stanów cywilnych",
+                FrontText = "",
+                BackSideTitle = "System zarządzania stanami cywilnymi",
+                BackSideText = "System zarządzania stanami cywilnymi pomoże w efektywnym monitorowaniu i organizowaniu informacji dotyczących stanu cywilnego pracowników.",
+                EntityName = "MaritalStatus",
+            },
+        };
+
+        return View(ret);
     }
 
     public IActionResult Privacy()
---
+++ HrHarmony/Data/Models/ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace HrHarmony.Data.Models.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        public string ImagePath { get; set; }
+
+        public string ImageAlt { get; set; }
+        
+        public string FrontTitle { get; set; }
+
+        public string FrontText { get; set; }
+
+        public string BackSideTitle { get; set; }
+
+        public string BackSideText { get; set; }
+
+        public string EntityName { get; set; }
+    }
+}
